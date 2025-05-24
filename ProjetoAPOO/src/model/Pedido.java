a44177babd3f1f70c5962e6233f21aee64df55a1
Wellington-Pc <wvfr@dicente.ifpe.edu.br>
2025-05-24 20:49:12 -0300

    Criando a parte "model"

--- ProjetoAPOO/src/model/Pedido.java
+++ ProjetoAPOO/src/model/Pedido.java
@@ -1,5 +1,56 @@
 package model;
 
-public class Pedido {
+import java.util.ArrayList;
+import java.util.List;
 
+public class Pedido {
+    private Cliente cliente;
+    private List<ItemPedido> itens;
+    private double valorFrete;
+    private double valorTotalPedido;
+
+    public Pedido(Cliente cliente) {
+        this.cliente = cliente;
+        this.itens = new ArrayList<>();
+    }
+
+    public void adicionarItem(ItemPedido item) {
+        this.itens.add(item);
+    }
+
+    public double calcularTotalProdutos() {
+        double total = 0.0;
+        for (ItemPedido item : itens) {
+            total += item.getSubtotal();
+        }
+        return total;
+    }
+
+    public double calcularPesoTotal() {
+        double pesoTotal = 0.0;
+        for (ItemPedido item : itens) {
+            pesoTotal += item.getPesoTotal();
+        }
+        return pesoTotal;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public List<ItemPedido> getItens() {
+        return itens;
+    }
+
+    public double getValorFrete() {
+        return valorFrete;
+    }
+
+    public void setValorFrete(double valorFrete) {
+        this.valorFrete = valorFrete;
+    }
+
+    public double getValorTotalPedido() {
+        return calcularTotalProdutos() + this.valorFrete;
+    }
 }
--- ProjetoAPOO/src/model/Produto.java
+++ ProjetoAPOO/src/model/Produto.java
@@ -1,5 +1,25 @@
 package model;
 
 public class Produto {
+    private String nome;
+    private double preco;
+    private double peso; // em kg
 
+    public Produto(String nome, double preco, double peso) {
+        this.nome = nome;
+        this.preco = preco;
+        this.peso = peso;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public double getPeso() {
+        return peso;
+    }
 }
